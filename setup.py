829424e109bb0206777c29df7d5d1ed8886983e5
maijinhao <maijhsz@sinosoft.com.cn>
2019-12-05 02:48:02 -0500

    xxx

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="dockerApi",
+    version="0.0.1",
+    description="dockerApi",
+    author="Mai",
+    include_package_data=True,
+    packages=find_packages(),
+)
+
+
